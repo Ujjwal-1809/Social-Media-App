c805c5368ae003d98a6b6a2bbe441b78a4462aaf
Ujjwal-1809 <ujjwalssharma12@gmail.com>
2025-02-14 16:06:22 +0530

    Cors origin changed 4

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -134,7 +134,7 @@ export async function forgotPassword(req, res) {
         await user.save();
 
         // Send email with reset link
-        const resetLink = `https://67af1543836d7a1dfda42acc--bespoke-fudge-fb649f.netlify.app/reset-password/${resetToken}`;
+        const resetLink = `https://67af1c01be350200082a8113--clinquant-daifuku-1623f6.netlify.app/reset-password/${resetToken}`;
         const mailOptions = {
             from: `Social-media-app ${process.env.EMAIL_USER}`,
             to: user.email,
--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "https://67af1543836d7a1dfda42acc--bespoke-fudge-fb649f.netlify.app",
+    origin: "https://67af1c01be350200082a8113--clinquant-daifuku-1623f6.netlify.app",
     credentials: true
 }))
 app.use('/auth', authRoutes);
